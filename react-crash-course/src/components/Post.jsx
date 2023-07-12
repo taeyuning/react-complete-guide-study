bbb2b90023186a49b8783392a6e445fb9202f5dc
taeyuning <taeyuning@gmail.com>
2023-07-12 20:01:23 +0900

    modify: Post

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,8 +1,14 @@
+const names = ["Maxminlian", "Manuel"];
+
 function Post() {
-return <div>
-    <p>Maxinilian</p>
-    <p>React.js is awesome</p>
-</div>
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+  return (
+    <div>
+      <p>{chosenName}</p>
+      <p>React.js is awesome</p>
+    </div>
+  );
 }
 
 export default Post;
